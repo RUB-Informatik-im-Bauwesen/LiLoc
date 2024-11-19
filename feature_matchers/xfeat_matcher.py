b9db9304afdb45549f7b5b7cd9bceea0bbd75a79
Patrick Herbers <patrick.herbers@rub.de>
2024-11-19 18:56:03 +0100

    Added xfeat+lightglue support

--- feature_matchers/xfeat_matcher.py
+++ feature_matchers/xfeat_matcher.py
@@ -9,12 +9,15 @@ import tqdm
 
 import cv2
 
+import logging
+log = logging.getLogger("LiLoc")
 
 class XFeatMatcher:
     def __init__(self, match_threshold=20, min_keypoints=10):
         self.feature_detector = XFeat()
         self.match_threshold = match_threshold
         self.min_keypoints = min_keypoints
+        self.use_lighterglue = True
 
     def find_features(self, img):
         if img.ndim > 3:
@@ -24,6 +27,9 @@ class XFeatMatcher:
         
         xc = self.feature_detector.detectAndCompute(img, None)
 
+        if self.use_lighterglue:
+            xc[0]["image_size"] = (img.shape[1], img.shape[0])
+            return xc[0], None
         if not batchmode:
             kpts1, descs1 = xc[0]['keypoints'], xc[0]['descriptors']
             return kpts1, descs1
@@ -33,11 +39,29 @@ class XFeatMatcher:
             return kpts, descs
     
     def match_points(self, pts1, des1, pts2, des2):
-        idx0, idx1 = self.feature_detector.match(des1, des2, 0.82)
-        points1 = pts1[idx0].cpu().numpy()
-        points2 = pts2[idx1].cpu().numpy()
+        if self.use_lighterglue:
+            points1, points2, good_matches = self.feature_detector.match_lighterglue(pts1, pts2, 0.82)
+            if len(points1) >= self.min_keypoints and len(points2) >= self.min_keypoints and len(good_matches > self.match_threshold):
+                H, inliers = cv2.findHomography(points1, points2, cv2.USAC_MAGSAC, 4., maxIters=700,
+                                                    confidence=0.995)
 
-        if len(points1) >= self.min_keypoints and len(points2) >= self.min_keypoints:
+                kp1 = [cv2.KeyPoint(p[0], p[1], 5) for p in points1]
+                kp2 = [cv2.KeyPoint(p[0], p[1], 5) for p in points2]
+                good_matches = [cv2.DMatch(i, i, 0) for i in range(len(good_matches))]
+
+                return list(zip(kp1, kp2)), good_matches, H
+            else:
+                log.debug("Match rejected with %d matches", len(good_matches))
+                return None, None, None
+        else:
+            idx0, idx1 = self.feature_detector.match(des1, des2, 0.82)
+            points1 = pts1[idx0].cpu().numpy()
+            points2 = pts2[idx1].cpu().numpy()
+
+            if len(points1) < self.min_keypoints or len(points2) < self.min_keypoints:
+                log.debug("Match rejected, not enough keypoints (%d)", min(len(points1), len(points2)))
+                return None, None, None
+
             # Find homography
             H, inliers = cv2.findHomography(points1, points2, cv2.USAC_MAGSAC, 4., maxIters=700,
                                                 confidence=0.995)
@@ -50,9 +74,8 @@ class XFeatMatcher:
             if len(good_matches) >= self.match_threshold:
                 return list(zip(kp1, kp2)), good_matches, H
             else:
+                log.debug("Match rejected with %d matches", len(good_matches))
                 return None, None, None
-        else:
-            return None, None, None
 
 
 
