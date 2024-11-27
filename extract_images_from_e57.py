5199ad4eb6769dd6a2eeb91af80933a51fffb681
Patrick Herbers <patrick.herbers@ruhr-uni-bochum.de>
2024-11-27 16:37:48 +0100

    extract_images_from_e57.py refactor

--- extract_images_from_e57.py
+++ extract_images_from_e57.py
@@ -1,3 +1,6 @@
+import os
+
+import cv2
 import pye57
 import json
 import numpy as np
@@ -6,17 +9,6 @@ import pye57.e57
 
 from helpers import NumpyArrayEncoder
 
-files = [
-    r"/home/patrick/dev/BIMKIT/straelen_2024-01-07/Job 009- HiWi Raum 003.e57",
-    r"/home/patrick/dev/BIMKIT/straelen_2024-01-07/Job 009- HiWi Raum 004.e57",
-    r"/home/patrick/dev/BIMKIT/straelen_2024-01-07/Job 009- HiWi Raum 005.e57",
-    r"/home/patrick/dev/BIMKIT/straelen_2024-01-07/Job 009- HiWi Raum 006.e57",
-    r"/home/patrick/dev/BIMKIT/straelen_2024-01-07/Job 009- HiWi Raum 007.e57",
-    r"/home/patrick/dev/BIMKIT/straelen_2024-01-07/Job 009- HiWi Raum 008.e57",
-    r"/home/patrick/dev/BIMKIT/straelen_2024-01-07/Job 009- HiWi Raum 009.e57",
-    r"/home/patrick/dev/BIMKIT/straelen_2024-01-07/Job 009- HiWi Raum 010.e57",
-    r"/home/patrick/dev/BIMKIT/straelen_2024-01-07/Job 009- HiWi Raum 011.e57",
-]
 
 def quaternion_to_euler(x, y, z, w):
     """
@@ -49,6 +41,7 @@ def quaternion_to_euler(x, y, z, w):
     
     return yaw, pitch, roll
 
+
 # ==============
 # e57 Extraction
 # ==============
@@ -62,6 +55,7 @@ def extract_pose(pc: pye57.E57):
     scanner_tr = header.translation
     return header.translation, header.rotation
 
+
 def extract_images(pc: pye57.E57):
     images = []
     parameters = []
@@ -73,42 +67,44 @@ def extract_images(pc: pye57.E57):
         camera = imNode["pinholeRepresentation"]
         i = camera["jpegImage"]
 
-        image = readImage(i)
+        image = read_image(i)
         images.append(image)
         imagesFound += 1
 
-        parameters.append(readImageParameters(imNode))
+        parameters.append(read_image_parameters(imNode))
         
     print(f"Found {imagesFound} images")
     return images, parameters
 
-def readImage(imageNode):
-    jpeg_image_data = np.zeros(shape=imageNode.byteCount(), dtype=np.uint8)
-    imageNode.read(jpeg_image_data, 0, imageNode.byteCount())
+
+def read_image(image_node):
+    jpeg_image_data = np.zeros(shape=image_node.byteCount(), dtype=np.uint8)
+    image_node.read(jpeg_image_data, 0, image_node.byteCount())
     image = cv.imdecode(jpeg_image_data, cv.IMREAD_COLOR)
     return image
 
-def readImageParameters(imageNode):
-    poseNode = imageNode["pose"]
+
+def read_image_parameters(image_node):
+    pose_node = image_node["pose"]
     pose = {
-        "rw": poseNode[0][0].value(),
-        "rx": poseNode[0][1].value(),
-        "ry": poseNode[0][2].value(),
-        "rz": poseNode[0][3].value(),
-        "tx": poseNode[1][0].value(),
-        "ty": poseNode[1][1].value(),
-        "tz": poseNode[1][2].value(),
+        "rw": pose_node[0][0].value(),
+        "rx": pose_node[0][1].value(),
+        "ry": pose_node[0][2].value(),
+        "rz": pose_node[0][3].value(),
+        "tx": pose_node[1][0].value(),
+        "ty": pose_node[1][1].value(),
+        "tz": pose_node[1][2].value(),
     }
     
-    cameraNode = imageNode["pinholeRepresentation"]
+    camera_node = image_node["pinholeRepresentation"]
     intrinsics = {
-        "focalLength":     cameraNode["focalLength"].value(),
-        "imageHeight":     cameraNode["imageHeight"].value(),
-        "imageWidth":      cameraNode["imageWidth"].value(),
-        "pixelHeight":     cameraNode["pixelHeight"].value(),
-        "pixelWidth":      cameraNode["pixelWidth"].value(),
-        "principalPointX": cameraNode["principalPointX"].value(),
-        "principalPointY": cameraNode["principalPointY"].value()
+        "focalLength":     camera_node["focalLength"].value(),
+        "imageHeight":     camera_node["imageHeight"].value(),
+        "imageWidth":      camera_node["imageWidth"].value(),
+        "pixelHeight":     camera_node["pixelHeight"].value(),
+        "pixelWidth":      camera_node["pixelWidth"].value(),
+        "principalPointX": camera_node["principalPointX"].value(),
+        "principalPointY": camera_node["principalPointY"].value()
     }
     intrinsics["sensorWidth"] = intrinsics["pixelWidth"] * intrinsics["imageWidth"]
     intrinsics["sensorHeight"] = intrinsics["pixelHeight"] * intrinsics["imageHeight"]
@@ -125,8 +121,8 @@ def readImageParameters(imageNode):
 # I/O
 # =======
 
-def write_camera_poses(poses):
-    with open("camera_poses.txt", "w") as f:
+def write_camera_poses(poses, fname):
+    with open(fname, "w") as f:
         for location in poses.values():
             for im in location["images"]:
                 pose = im["pose"]
@@ -134,42 +130,73 @@ def write_camera_poses(poses):
                 f.write(f"{pose['tx']} {pose['ty']} {pose['tz']} {yaw} {pitch} {roll}\n")
 
 
-pose_dict = {}
-
-for path in files:
-    name = path.split("/")[-1]
-    name = name.split(".")[0]
-    pc_e57 = pye57.E57(path)
-    pose_tr, pose_rot = extract_pose(pc_e57)
-    images, image_parameters = extract_images(pc_e57)
+def extract_e57_pose(file, *args, write_imgs=False):
+    pose_dict = {}
     image_dict = {}
 
-    if(len(images) == 6):
-        image_parameters[0]["facing"] = "00_front" 
-        image_parameters[1]["facing"] = "01_right" 
-        image_parameters[2]["facing"] = "02_back"  
-        image_parameters[3]["facing"] = "03_left"  
-        image_parameters[4]["facing"] = "04_up"    
-        image_parameters[5]["facing"] = "05_down"  
+    filelist = [file]
 
-    pose_dict[name] = {
-        "pose":
-        {
-            "translation": pose_tr,
-            "rotation": pose_rot
-        },
-        "images": image_parameters
-    }
+    filelist.extend(args)
 
-    # print(image_parameters)
-    
-    for i, (image, image_p) in enumerate(zip(images, image_parameters)):
-        fname = f"{name}_{image_p.get('facing', i)}.jpg"
-        cv.imwrite(f"images/{fname}", image)
-        image_p["file"] = fname
+    for path in filelist:
+        if path is not pye57.E57:
+            name = path.split(os.path.sep)[-1]
+            name = name.split(".")[0]
+            pc_e57 = pye57.E57(path)
+        else:
+            pc_e57: pye57.E57 = path
+            name = path.split(pc_e57.path)[-1]
+            name = name.split(".")[0]
 
-with open("scanner_poses.json", "w") as f:
-    json.dump(pose_dict, f, cls=NumpyArrayEncoder, indent=2)
+        pose_tr, pose_rot = extract_pose(pc_e57)
+        images, image_parameters = extract_images(pc_e57)
 
-write_camera_poses(pose_dict)
+        if len(images) == 6:
+            image_parameters[0]["facing"] = "00_front"
+            image_parameters[1]["facing"] = "01_right"
+            image_parameters[2]["facing"] = "02_back"
+            image_parameters[3]["facing"] = "03_left"
+            image_parameters[4]["facing"] = "04_up"
+            image_parameters[5]["facing"] = "05_down"
 
+        pose_dict[name] = {
+            "pose":
+            {
+                "translation": pose_tr,
+                "rotation": pose_rot
+            },
+            "images": image_parameters
+        }
+
+        # print(image_parameters)
+
+        for i, (image, image_p) in enumerate(zip(images, image_parameters)):
+            fname = f"{name}_{image_p.get('facing', i)}.jpg"
+            if write_imgs:
+                cv.imwrite(f"images/{fname}", image)
+            image_dict[fname] = image
+            image_p["file"] = fname
+
+    with open("scanner_poses.json", "w") as f:
+        json.dump(pose_dict, f, cls=NumpyArrayEncoder, indent=2)
+
+    return pose_dict, image_dict
+
+
+if __name__ == "__main__":
+    files = [
+        r"E:\Seafile\RTC360\01-Parkplatz.e57                                ",
+        r"E:\Seafile\RTC360\02-Unterbau-Korrosion.e57                       ",
+        r"E:\Seafile\RTC360\03-Unterbau-Korrosion-retry.e57                 ",
+        r"E:\Seafile\RTC360\04-Unterbau-Durchfeuchtung.e57                  ",
+        r"E:\Seafile\RTC360\05-Unterbau-Durchfeuchtung.e57                  ",
+        r"E:\Seafile\RTC360\06-Unterbau-Risse.e57                           ",
+        r"E:\Seafile\RTC360\07-Unterbau-Durchfeuchtung.e57                  ",
+        r"E:\Seafile\RTC360\08-Unterbau-Durchfeuchtung-Risse-Grafitti.e57   ",
+        r"E:\Seafile\RTC360\09-Trasse.e57                                   ",
+    ]
+
+    poses, images = extract_e57_pose(*files)
+    for imgname, img in images.items():
+        cv2.imwrite("data/a44/" + imgname, img)
+    write_camera_poses(poses, "data/a44/camera_poses.txt")
